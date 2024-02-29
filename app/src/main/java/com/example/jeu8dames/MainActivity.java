3026f99e316c1f0f51f08e37671a3edeec3d8674
TP User <tpuser@m-ulr-u20-devand.univ-lr.fr>
2024-02-29 12:48:29 +0100

    test

--- app/src/main/java/com/example/jeu8dames/MainActivity.java
+++ app/src/main/java/com/example/jeu8dames/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.jeu8dames;
+package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.win_layout);
     }
 }
