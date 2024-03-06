e32673ecd829576abc15510a1e967d59cb747fa4
TP User <tpuser@m-ulr-u20-devand.univ-lr.fr>
2024-03-06 10:59:58 +0100

    nouvelles interfaces

--- app/src/main/java/com/example/jeu8dames/MainActivity.java
+++ app/src/main/java/com/example/jeu8dames/MainActivity.java
@@ -1,14 +1,23 @@
-package com.example.jeu8dames;
+package com.example.jeu8dames; // Remplacez par le nom de votre package
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.ecran_introduction);
     }
+
+    // Cette méthode est appelée lorsque le bouton est cliqué.
+    public void start(View view) {
+        Intent intent = new Intent(this, JeuPrincipal.class);
+        startActivity(intent);
+    }
+
 }
---
+++ app/src/main/java/com/example/jeu8dames/Victoire.java
@@ -0,0 +1,14 @@
+package com.example.jeu8dames; // Remplacez par le nom de votre package
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Victoire extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.win_layout);
+    }
+}
