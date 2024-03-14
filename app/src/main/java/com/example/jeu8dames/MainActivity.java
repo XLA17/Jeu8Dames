3cd548ebfb99516a2a9480773c50c290db6597b2
Alexy Lafosse <alexylafosse@MacBook-Air-de-Alexy.local>
2024-03-14 12:15:16 +0100

    changement fonctionnement victoire

--- app/src/main/java/com/example/jeu8dames/MainActivity.java
+++ app/src/main/java/com/example/jeu8dames/MainActivity.java
@@ -17,7 +17,7 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
 
     List<int[]> victoryPossibilities = new ArrayList<>();
-    int[] currentPossibility = {0, 0, 0, 0, 0, 0, 0, 0};
+    int[] currentPossibility = {-1,-1,-1,-1,-1,-1,-1,-1};
     TableLayout tl;
     int countQueen = 0;
 
@@ -28,44 +28,61 @@ public class MainActivity extends AppCompatActivity {
 
         tl = findViewById(R.id.tableLayout);
 
-        int[] p1 = {4,9,21,32,34,47,51,62};
-        int[] p2 = {7,12,18,32,38,41,51,61};
-        int[] p3 = {3,14,18,31,33,44,56,61};
-        int[] p4 = {4,14,24,27,33,47,53,58};
-        victoryPossibilities.add(p1);
-        victoryPossibilities.add(p2);
-        victoryPossibilities.add(p3);
-        victoryPossibilities.add(p4);
+        int[] s1 = {1,3,5,7,2,0,6,4};
+        int[] s2 = {0,6,3,5,7,1,4,2};
+        int[] s3 = {0,6,4,7,1,3,5,2};
+        int[] s4 = {3,0,4,7,1,6,2,5};
+        int[] s5 = {4,0,7,3,1,6,2,5};
+        int[] s6 = {2,0,6,4,7,1,3,5};
+        int[] s7 = {4,0,3,5,7,1,6,2};
+        int[] s8 = {6,0,2,7,5,3,1,4};
+        int[] s9 = {4,0,7,5,2,6,1,3};
+        int[] s10 = {4,6,0,3,1,7,5,2};
+        int[] s11 = {5,2,0,7,3,1,6,4};
+        int[] s12 = {4,2,0,6,1,7,5,3};
+
+        victoryPossibilities.add(s1);
+        victoryPossibilities.add(s2);
+        victoryPossibilities.add(s3);
+        victoryPossibilities.add(s4);
+        victoryPossibilities.add(s5);
+        victoryPossibilities.add(s6);
+        victoryPossibilities.add(s7);
+        victoryPossibilities.add(s8);
+        victoryPossibilities.add(s9);
+        victoryPossibilities.add(s10);
+        victoryPossibilities.add(s11);
+        victoryPossibilities.add(s12);
 
         for (int i = 0; i < 8; i++) {
             TableRow row = new TableRow(this);
             TableRow.LayoutParams layoutParams = new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT, TableRow.LayoutParams.WRAP_CONTENT);
             row.setLayoutParams(layoutParams);
 
-            for (int j = 1; j <= 8; j++) {
+            for (int j = 0; j < 8; j++) {
                 ImageView imageView = new ImageView(this);
-                int index = 8 * i + j;
                 int colorRes = (i % 2 == 0) ? ((j % 2 == 0) ? R.color.beige : R.color.brown)
                         : ((j % 2 == 0) ? R.color.brown : R.color.beige);
                 imageView.setBackgroundColor(getResources().getColor(colorRes));
                 imageView.setLayoutParams(new TableRow.LayoutParams(100, 100));
+                int colonne = j;
+                int ligne = i;
                 imageView.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
                         if (countQueen < 8) {
                             if (imageView.getDrawable() == null) {
-                                currentPossibility[countQueen] = index;
+                                currentPossibility[colonne] = ligne;
                                 countQueen++;
                                 imageView.setImageResource(R.drawable.queen);
                             }
                             else {
-                                currentPossibility[countQueen] = 0;
+                                currentPossibility[colonne] = 0;
                                 countQueen--;
                                 imageView.setImageDrawable(null);
                             }
                         }
                         if (countQueen == 8){
-                            boolean isVictory;
                             if (isArrayInListArray(victoryPossibilities, currentPossibility)){
                                 Intent intent = new Intent(MainActivity.this, WinLooseActivity.class);
                                 intent.putExtra("isVictory", true);
