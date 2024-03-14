9890f1ad0f230e15b8f00a4f520eddf421da928d
Alexy Lafosse <alexylafosse@MacBook-Air-de-Alexy.local>
2024-03-14 09:10:09 +0100

    correction bug redemarrer

--- app/src/main/java/com/example/jeu8dames/WinLooseActivity.java
+++ app/src/main/java/com/example/jeu8dames/WinLooseActivity.java
@@ -1,7 +1,12 @@
 package com.example.jeu8dames;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 
@@ -9,6 +14,10 @@ public class WinLooseActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.win_loose_layout);
+
+        TextView textView = findViewById(R.id.message);
+        Button restartButton = findViewById(R.id.restartButton);
 
         Intent intent = getIntent();
 
@@ -16,12 +25,19 @@ public class WinLooseActivity extends AppCompatActivity {
             boolean isVictory = intent.getBooleanExtra("isVictory", false);
 
             if (isVictory){
-                setContentView(R.layout.win_layout);
+                textView.setText("Bravo ! Vous avez gagné !");
             }
             else {
-                setContentView(R.layout.loose_layout);
+                textView.setText("Vous avez perdu !");
             }
         }
+
+        restartButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                recommencer();
+            }
+        });
     }
 
     public void recommencer(){
